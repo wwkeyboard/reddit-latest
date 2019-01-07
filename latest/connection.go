9ec8e3f5355e15eba1d1cd3125c08e39959c50d1
Aaron Lee <alee@aaronosaur.us>
2019-01-06 20:55:24 -0600

    list sub headdings

--- latest/connection.go
+++ latest/connection.go
@@ -3,11 +3,29 @@ package latest
 import (
 	"fmt"
 
+	"github.com/jzelinskie/geddit"
+
 	"github.com/spf13/cobra"
 )
 
 // RunE is the entry poing for the application
 func RunE(cmd *cobra.Command, args []string) error {
 	fmt.Println("runing")
+
+	session := geddit.NewSession("AaronsLonelyPostFinder")
+
+	subOpts := geddit.ListingOptions{
+		Limit: 10,
+	}
+
+	subs, err := session.SubredditSubmissions("photoclass2019", geddit.TopSubmissions, subOpts)
+	if err != nil {
+		return err
+	}
+
+	for _, s := range subs {
+		fmt.Printf("-- %v, \n", s.Selftext)
+	}
+
 	return nil
 }
