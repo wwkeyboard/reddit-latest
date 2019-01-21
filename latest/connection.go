a8a87f1670ed49ff682180b4d2c3375716599496
Aaron Lee <alee@aaronosaur.us>
2019-01-21 14:10:47 -0600

    latest threads from subreddit

--- latest/connection.go
+++ latest/connection.go
@@ -10,21 +10,19 @@ import (
 
 // RunE is the entry poing for the application
 func RunE(cmd *cobra.Command, args []string) error {
-	fmt.Println("runing")
-
 	session := geddit.NewSession("AaronsLonelyPostFinder")
 
 	subOpts := geddit.ListingOptions{
 		Limit: 10,
 	}
 
-	subs, err := session.SubredditSubmissions("photoclass2019", geddit.TopSubmissions, subOpts)
+	topics, err := session.SubredditSubmissions("photoclass2019", geddit.NewSubmissions, subOpts)
 	if err != nil {
 		return err
 	}
 
-	for _, s := range subs {
-		fmt.Printf("-- %v, \n", s.Selftext)
+	for _, t := range topics {
+		fmt.Printf("%+v\n", t.Title)
 	}
 
 	return nil
